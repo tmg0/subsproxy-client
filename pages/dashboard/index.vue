46b28cfac759a78cb7ca49f5d8ed35b4cf0a7821
zekunjin <zekun.jin@foxmail.com>
2023-07-07 18:02:37 +0800

    feat: add dashboard page

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import Menu from '~icons/carbon/menu'
+import Search from '~icons/carbon/search'
+
 const { data } = useAccounts()
 </script>
 
 <template>
-  <div class="p-4 flex flex-col gap-2">
+  <div class="w-screen h-screen flex flex-col gap-2 bg-[#f2f2f2]">
+    <div class="p-4 flex w-full items-center justify-between">
+      <button class="btn btn-circle">
+        <Menu />
+      </button>
+
+      <button class="btn btn-circle">
+        <Search />
+      </button>
+    </div>
+
     <div v-for="account in data" :key="account.id" class="rounded shadow">
       #{{ account.username }}
     </div>
