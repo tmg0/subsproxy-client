bd5a61659055a5ca1fd31eac295206429dcd1fdc
zekunjin <zekun.jin@foxmail.com>
2023-07-12 16:05:31 +0800

    style: update in active status dot

--- components/Account/Card.vue
+++ components/Account/Card.vue
@@ -7,7 +7,7 @@ const emit = defineEmits(['click'])
 </script>
 
 <template>
-  <div :key="account.id" class="card shadow-xl cursor-pointer bg-white border-r-2 border-solid border-r-accent" @click="emit('click')">
+  <div :key="account.id" class="card shadow-xl cursor-pointer bg-white" @click="emit('click')">
     <div class="p-3 flex items-center gap-4">
       <div class="avatar placeholder">
         <div class="font-bold text-2xl w-12 rounded-xl bg-gradient-to-br from-sky-500 to-primary text-white uppercase">
@@ -18,8 +18,12 @@ const emit = defineEmits(['click'])
       <div class="h-12 text-sm flex-1 flex justify-between">
         <div class="h-full flex flex-col gap-2 justify-center">
           <div class="flex items-center gap-2">
+            <div class="w-2 h-2 bg-accent/30 rounded-full flex items-center justify-center">
+              <div class="w-1 h-1 bg-accent rounded-full" />
+            </div>
+
             <div class="font-bold uppercase">
-              # {{ account.username }}
+              {{ account.username }}
             </div>
 
             <div class="text-xs text-gray-300">
