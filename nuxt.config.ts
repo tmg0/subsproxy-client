6e9a9457dc7190fb0b8da8160f93180deb222ee1
zekunjin <zekun.jin@foxmail.com>
2023-07-18 10:21:51 +0800

    chore: add client title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Subsproxy'
+    }
+  },
+
   devtools: { enabled: true },
+
   modules: ['@nuxtjs/tailwindcss', 'unplugin-icons/nuxt', '@vueuse/motion/nuxt'],
+
   imports: { dirs: ['./stores'] },
+
   vite: {
     server: {
       proxy: {
