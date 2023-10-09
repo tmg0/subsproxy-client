0d3b99fcbf89539b17aa864716efe2eec812ec0c
zekunjin <zekun.jin@foxmail.com>
2023-10-09 16:10:59 +0800

    chore: use https

--- components/Account/Avatar.vue
+++ components/Account/Avatar.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { withHttps } from 'ufo'
+
 interface Props {
   content?: string
   src?: string
@@ -9,7 +11,7 @@ const props = defineProps<Props>()
 
 <template>
   <div class="avatar placeholder relative w-12 h-12 rounded-xl overflow-hidden">
-    <NuxtImg v-if="src" :src="src" class="block" />
+    <NuxtImg v-if="src" :src="withHttps(src)" class="block" />
     <div v-if="!src" class="font-bold text-2xl w-full h-full bg-gradient-to-br from-sky-500 to-primary text-white uppercase">
       <span>{{ props.content?.split('.').at(-1)?.at(0) ?? '' }}</span>
     </div>
